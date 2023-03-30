e6bd83ba875c9d5d6b3ea32a13aa9fa2f7b5e5dc
yabluko <yurarobak7@gmail.com>
2023-03-30 22:10:20 +0300

    update

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def core(request):
-    return render(request, './core-index.html')
+    return render(request, 'core-index.html')
--- info/views.py
+++ info/views.py
@@ -4,4 +4,4 @@ from core.models import Account
 # Create your views here.
 def info(request):
     account_list = Account.objects.all()
-    return render(request, './info-index.html', {"accounts":account_list})
+    return render(request, 'info-index.html', {"accounts":account_list})
