cd849a1a2d525fa3a3380b07f10c2f3042144325
yabluko <yurarobak7@gmail.com>
2023-04-06 10:03:27 +0300

    home+signup

--- registrationapp/views.py
+++ registrationapp/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
 from django.shortcuts import render , redirect
 from core.models import Account
+# from django.contrib import messages
+# from django.contrib.auth import authenticate,login
 # Create your views here.
 
 
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
         email = request.POST['email']
-        password = request.POST['password']
-
+        password = request.POST['password'] 
+        # user = authenticate(request, username=username , email=email, password=password)
         # user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, password=password)
  
         # user_model = User.objects.get(username=username)
         new_profile = Account.objects.create(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+
+        # if user is not None:
+        #     # A backend authenticated the credentials
+        #     login(request, user)
+        #     return redirect('')
+        # else:
+        #     messages.success(request,)
+    # No backend authenticated the credentials
         return redirect('login/')
-    
     return render(request, 'registration-index.html')
 
 # def login(request):
