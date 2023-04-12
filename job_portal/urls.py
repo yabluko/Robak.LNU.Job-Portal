d2a95ed1570e83cdac23a44974ac776496663f97
yabluko <yurarobak7@gmail.com>
2023-04-12 15:04:19 +0300

    upgrades

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('signup/', include('registrationapp.urls')),
+    path('registration/', include('registrationapp.urls')),
 ]
  
--- registrationapp/views.py
+++ registrationapp/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import render , redirect
 from core.models import Account
-# from django.contrib import messages
-# from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password'] 
-        # user = authenticate(request, username=username , email=email, password=password)
-        # user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, password=password)
+def signin(request):
+    # if request.method == 'POST':
+    #     username = request.POST['username']
+    #     email = request.POST['email']
+    #     password = request.POST['password'] 
+    #     user = authenticate(request, username=username , email=email, password=password)
+        # user = User.objects.create_user(username=username, email=email, password=password)
  
         # user_model = User.objects.get(username=username)
-        new_profile = Account.objects.create(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+        # new_profile = Account.objects.create(email=email, username=username, password=password)
 
         # if user is not None:
         #     # A backend authenticated the credentials
@@ -24,8 +24,24 @@ def signup(request):
         # else:
         #     messages.success(request,)
     # No backend authenticated the credentials
-        return redirect('login/')
-    return render(request, 'registration-index.html')
+        # return redirect('login/')
+    return render(request, 'signin.html')
 
-# def login(request):
-#     return render(request, 'registrationapp/login.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        firstname = request.POST['first_name']
+        lastname = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        myuser = Account.objects.create(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been succesfully created")
+        return redirect('/registration/signin')
+
+    return render(request, 'signup.html')
