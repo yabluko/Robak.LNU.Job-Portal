e0b63c8b36e389abdeb3d47d987bf12e5d191545
yabluko <yurarobak7@gmail.com>
2023-05-04 00:02:47 +0300

    added following users

--- registrationapp/views.py
+++ registrationapp/views.py
@@ -6,20 +6,21 @@ from .forms import PostForm
 # Create your views here.
 
 def main(request):
-    # if request.method == 'POST':
-    #     email = request.POST['email']
-    #     password = request.POST['password']
-    #     user = authenticate(request, email=email, password=password)
-                
-    #     if user is not None:
-    #         login(request, user)
-    #         messages.success(request, ("You have been succesfully log in"))
-    #         return redirect('main')
-    #     else:
-    #         messages.success(request,("There was an error logging in .Please try again..."))
-    #         return redirect('home')
-    # else:    
-    return render(request, 'initial-page.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+                 
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been succesfully log in"))
+            return redirect('home')
+        else:
+            messages.success(request,("There was an error logging in .Please try again..."))
+            return redirect('main')
+        
+    else:    
+        return render(request, 'initial-page.html')
 
 
 def signin(request):
@@ -32,8 +33,29 @@ def signup(request):
 
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'profile-page.html', {"profiles": profiles})
+    return render(request, 'profile-list.html', {"profiles": profiles})
     
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+
+        if request.method == 'POST':
+            #Get current user
+            current_user_profile = request.user.profile
+            #Get form data
+            action = request.POST['follow']
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()            
+
+        return render(request,'profile-user.html', {'profile':profile} )
+    else:
+        messages.success(request, ('You must be logged in '))
+        return redirect('main')
+
  
 def home(request):
     if request.user.is_authenticated:
@@ -49,8 +71,9 @@ def home(request):
         posts = Post.objects.all().order_by("-created_at")
         return render(request, 'home-page.html', {'posts': posts, 'form': form})
     else:
+        messages.success(request, "Your must be log in!")
         posts = Post.objects.all().order_by("-created_at")
-        return render(request, 'home-page.html', {'posts': posts})
+    return render(request, 'home-page.html',{'posts': posts})
     
 
 # def profile(request):
