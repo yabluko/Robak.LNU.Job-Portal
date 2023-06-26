e071113ce94651120d9aa4a2dd297c451e0b6a14
yabluko <yurarobak7@gmail.com>
2023-06-26 16:29:44 +0300

    hover added

--- static/js/home-page.js
+++ static/js/home-page.js
@@ -1,9 +1,9 @@
-const divElement = document.querySelectorAll('.leftside__list__li');
-console.log(divElement);
+// const divElement = document.querySelectorAll('.leftside__list__li');
+// console.log(divElement);
 
 
-divElement.forEach(div => {
-    div.addEventListener('click', function handleCliker(event){
-        event.currentTarget.style.backgroundColor = '#0073b138';
-    });
-})
+// divElement.forEach(div => {
+//     div.addEventListener('click', function handleCliker(event){
+//         event.currentTarget.style.backgroundColor = '#0073b138';
+//     });
+// })
